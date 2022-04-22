190fa49777e910fd48d38f76ab40023ccb668c5c
Andy Kukuc <andykukuc@protonmail.com>
2022-04-21 20:47:04 -0500

    added the next project

---
+++ FormattingStringsandComments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormattingStringsandComments
+{        
+    ///<summary>
+    ///Documentation comments go before a method or a class bro.
+    ///IIT never taught me this FUCK YOU PROFESSOR BILLY YOU'RE A BITCH
+    ///</summary>
+    class Program //class
+
+    {
+        static void Main(string[] args) //method
+        {
+            /*MULTIPLE LINES*/
+            //Single Lines
+        }
+    }
+}
