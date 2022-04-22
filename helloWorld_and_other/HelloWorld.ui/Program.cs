1222f7b6970aa940bcf1eb5d7d988fb5c4580324
Andy Kukuc <jeep98.ak@gmail.com>
2022-04-21 19:38:59 -0500

    Now that I got github all set. Added comments and imported several namespaces.

--- helloWorld_and_other/HelloWorld.ui/Program.cs
+++ helloWorld_and_other/HelloWorld.ui/Program.cs
@@ -1,15 +1,20 @@
 ﻿//Andy "shad0wMast3r" Kukuc
 //The beginning of the end.
+//Importing the Collection of Libraries (Namespaces)
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HelloWorld.ui
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace HelloWorld.ui 
+{ //belongs to the containing statment
+    class Program //class definition
+    { //Class Members 
+        static void Main(string[] args)//Main method. Also special because this is where it all starts
+        {//methods are the verbs of programming.
+            Console.WriteLine("Hello World!"); //Print out the message
             Console.ReadLine(); //Read the Users Input.
-        }
-    }
-}
+        }//Main method
+    }//Class Members 
+}//belongs to the containing statment
