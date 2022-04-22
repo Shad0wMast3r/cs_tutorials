d1b05528c64b8f6ec021e8e0dcd4faaf8834297e
Andy Kukuc <andykukuc@protonmail.com>
2022-04-21 20:15:16 -0500

    more sample coding. asking for user input and then printing it to the console.

---
+++ HelloName/HelloName.ui/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName.ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the for the Users Name and ReadLine does magics
+            Console.Write("What is your name? ");
+            //Declare the String to Store Name
+            string name = Console.ReadLine(); //the best way saves time and money
+            //Print out message
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
+        }
+    }
+}
