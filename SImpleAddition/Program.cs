6aad4e18b4558ca5d1620d0e06abdbd92784f7bf
Andy Kukuc <andykukuc@protonmail.com>
2022-04-21 20:36:54 -0500

    vastly superirityizing the code

---
+++ SImpleAddition/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SImpleAddition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MATHS IN C#
+            Console.Write("Please enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine()); //Superiority
+            Console.Write("Please enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine()); //Superiority
+            //int sum = num1 + num2; DEPRICATE
+            Console.WriteLine("The sum is: "+(num1+num2)); //SUPERIOR
+        }
+    }
+}
