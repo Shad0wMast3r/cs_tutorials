4abf37cdefa65677044e3ff1c1b3eb535f84c737
Andy Kukuc <andykukuc@protonmail.com>
2022-04-25 20:13:53 -0500

    Expressions and Literals

---
+++ ExpressionsandOperators/ExpressionsandOperators/Program.cs
@@ -0,0 +1,7 @@
+﻿/*Literals Examples*/
+Console.WriteLine("{0}", 1234); //this is int literal
+Console.WriteLine("{0}", 3.14); //double literal
+Console.WriteLine("{0}",3.14F); //floating literal
+Console.WriteLine("{0}",true); //Boolean Literal
+Console.WriteLine("{0}",'a'); //char literal
+Console.WriteLine("{0}","Bitch!"); //string literal
